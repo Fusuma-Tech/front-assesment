e6215b62b34ab100ce9a5456845af7e5c4ddfb02
Christian <c.ciudadparedes@gmail.com>
2022-05-04 08:20:46 +0200

    Change app remove text

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Frontend Assesment - Fusuma</p>
+        <p>Frontend Assesment - by Christian</p>
       </header>
       <BrowserRouter>
       <div className="App-body">
